1149818b3e8eaf6b9c710640c1559573298714b9
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2024-05-13 14:16:59 +0200

    Input with wheel

--- DeepDive/Assets/Chris/Scripts/DriveController.cs
+++ DeepDive/Assets/Chris/Scripts/DriveController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class DriveController : MonoBehaviour
@@ -17,44 +18,23 @@ public class DriveController : MonoBehaviour
     public float moveSpeed = 5f;
     public float rotationSpeed = 100f;
 
+    PlayerInput playerInput;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        vertical = Input.GetAxis("Horizontal");
-        horizontal = Input.GetAxis("Vertical");
-        
-        Vector3 velocity = (transform.forward * vertical) * moveSpeed * Time.fixedDeltaTime;
-        float newSpeed = moveSpeed;
-        if (newSpeed > 0)
-        {
-            if (Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") == 0)
-            {
-                velocity = (transform.forward * vertical) * (newSpeed * 100) * Time.fixedDeltaTime;
-            }
-            if (Input.GetAxis("Vertical") != 0 && Input.GetAxis("Horizontal") == 0)
-            {
-                velocity = (transform.right * horizontal) * (newSpeed * 100) * Time.fixedDeltaTime;
-            }
-            if (Input.GetAxis("Vertical") != 0 && Input.GetAxis("Horizontal") != 0)
-            {
-                velocity = (transform.right * horizontal + transform.forward * vertical) * (newSpeed * 100) * Time.fixedDeltaTime;
-            }
-        }
-        velocity.y = rb.velocity.y;
-        rb.velocity = velocity;
-        */
 
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        horizontal = playerInput.actions["SteeringWheel"].ReadValue<float>();
+        vertical = playerInput.actions["GasBrake"].ReadValue<float>();
 
 
         Vector3 moveDirection = transform.forward * vertical;
