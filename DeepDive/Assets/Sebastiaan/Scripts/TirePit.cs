f66a14320f9e34eb28d4f5ec0e8f1ba4265e5ed6
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2024-05-16 10:14:53 +0200

    Wheels probably done?

--- DeepDive/Assets/Sebastiaan/Scripts/TirePit.cs
+++ DeepDive/Assets/Sebastiaan/Scripts/TirePit.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TirePit : MonoBehaviour
 {
     public GameObject TireUi;
-
+    private CartTest player;
+    float normalBrake;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,12 @@ public class TirePit : MonoBehaviour
         if(other.tag == "Player")
         {
             TireUi.SetActive(true);
+            FindAnyObjectByType<EventSystem>().SetSelectedGameObject(TireUi.transform.GetChild(0).gameObject);
+            player = other.gameObject.GetComponent<CartTest>();
+            player.canMove = false;
+            player.forceBrake = true;
+            normalBrake = player.brakeTorque;
+            player.brakeTorque = 9999999999999999;
         }
     }
 
@@ -31,6 +39,21 @@ public class TirePit : MonoBehaviour
         //1 soft, 2 med, 3 hard
         //change tire on car
         Debug.Log(tireChoice);
+        switch (tireChoice)
+        {
+            case 1:
+                player.SwapWheels(CartTest.wheelTypes.Soft);
+                break;
+            case 2:
+                player.SwapWheels(CartTest.wheelTypes.Medium);
+                break;
+            case 3:
+                player.SwapWheels(CartTest.wheelTypes.Hard);
+                break;
+        }
         TireUi.SetActive(false);
+        player.canMove = true;
+        player.forceBrake = false;
+        player.brakeTorque = normalBrake;
     }
 }
