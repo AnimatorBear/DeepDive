45b1d4ca55062ebac31ce80fea2c8546b497a23d
Lucifixr <twp144@gmail.com>
2024-05-16 11:44:26 +0200

    kleine tweak
    yes

--- DeepDive/Assets/Luci/Scripts/CarUI.cs
+++ DeepDive/Assets/Luci/Scripts/CarUI.cs
@@ -7,45 +7,34 @@ using static UnityEngine.GraphicsBuffer;
 public class CarUI : MonoBehaviour
 {
     public Rigidbody carTarget;
-    public DriveController carController;
+    public CartTest carController;
     private float maxSpeed;
     private float currentSpeed;
     private int currentShift;
 
-    public float minSpeedArrow;
-    private float maxSpeedArrow;
+    public float minSpeedArrowAngle;
+    public float[] maxSpeedArrowAngle;
 
     [Header("UI")]
     public TMP_Text speedLabel;
     public RectTransform arrow;
     public TMP_Text gearLabel;
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         maxSpeed = carController.maxspeed;
         currentShift = carController.currentShift;
-        maxSpeedArrow = maxSpeed;
-        
         // De 3.6f convert het naar km/h
-        currentSpeed = carTarget.velocity.magnitude * 3.6f;
-        
+        currentSpeed = carTarget.velocity.magnitude;
+
         if (gearLabel != null )
         {
             gearLabel.text = currentShift.ToString();
         }
-        if (speedLabel != null )
-        {
-            speedLabel.text = ((int)currentSpeed) + "km/h";
-        }
         if (arrow != null )
         {
-            arrow.localEulerAngles = new Vector3(0, 0, Mathf.Lerp(minSpeedArrow, maxSpeedArrow, currentSpeed / maxSpeed));
+            arrow.localEulerAngles = new Vector3(0, 0, Mathf.Lerp(minSpeedArrowAngle, maxSpeedArrowAngle[currentShift], currentSpeed / maxSpeed));
         }
-
     }
 }
